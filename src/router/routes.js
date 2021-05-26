0100ad402555bd53fe087b23a4cd7b484fd7f3e5
Alex Ross <arossg17@gmail.com>
2021-05-25 20:46:54 -0400

    Fix routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
-
-const routes = [
-	{
-	  path: '/underconstruction',
-	  component: Foo
-	}
-  ]
-  
-  export default routes
-  
+export const routes = [
+  {
+    path: '/',
+    name: 'PokemonPicker',
+    component: () => import('../components/PokemonPicker.vue'),
+  },
+  {
+    path: '/pokemon/:slug',
+    name: 'Pokemon',
+    component: () => import('../components/Pokemon.vue'),
+  },
+]
