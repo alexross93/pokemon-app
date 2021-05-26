d32fa8f45419601ba62d1c6a65579769907d6acd
Alex Ross <arossg17@gmail.com>
2021-05-25 20:45:45 -0400

    Add filter to pokemon picker

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>This is the Pokemon page</h1>
+		<h3>{{$route.params.slug}}</h3>
+	</div>
+</template>
+
+<script>
+import { reactive, toRefs, computed } from 'vue'
+
+export default {
+  name: 'Pokemon',
+  components: {}
+}
+
+</script>
--- src/components/PokemonPicker.vue
+++ src/components/PokemonPicker.vue
@@ -1,28 +1,44 @@
 <template>
   <div class="w-full flex justify-center">
-    <input type="text" placeholder="Enter Pokemon here" class="mt-10 p-2 border-blue-500 border-2" />
+    <input
+      v-model="name"
+      type="text"
+      placeholder="Enter Pokemon here"
+      class="mt-10 p-2 border-blue-500 border-2"
+    />
   </div>
   <div class="mt-10 p-4 flex flex-wrap justify-center">
     <div
       class="ml-4 text-2x text-blue-200"
-      v-for="(pokemon, index) in pokemons"
+      v-for="(pokemon, index) in filteredPokemon"
       :key="index"
-    >{{pokemon.name}}</div>
+    >
+      <router-link :to="`/pokemon/${urlIdLookup[pokemon.name]}`">{{pokemon.name}}</router-link>
+    </div>
   </div>
   <!-- <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>-->
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, computed } from 'vue'
 
 export default {
-  name: 'Dashboard',
+  name: 'PokemonPicker',
   setup () {
 
     const state = reactive({
       pokemons: [],
-      urlIdLookup: {}
+      urlIdLookup: {},
+      name: '',
+      filteredPokemon: computed(() => updatePokemon())
     })
+
+    function updatePokemon () {
+      if (!state.name) {
+        return []
+      }
+      return state.pokemons.filter((pokemon) => pokemon.name.includes(state.name))
+    }
 
     fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
       .then((res) => res.json())
